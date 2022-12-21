ecdc912fbb646fd3872e791f591049608c32125a
Ilvikamittal <112233721+Ilvikamittal@users.noreply.github.com>
2022-12-21 17:37:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song="";
+
+function preload() {
+song=loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(550, 400);
+    canvas.center();
+
+   video=createCapture(VIDEO);
+   video.hide(); 
+}
+
+function draw() {
+    image(video, 0, 0, 550, 400);
+}
+
+function play(){
+    song.play();
+}
